a972a396de33ee2fc788fc647a49ab04e8016558
Samrath Jhand <you@example.com>
2022-05-23 15:58:35 -0700

    e

--- chatscript.js
+++ chatscript.js
@@ -1,9 +1,11 @@
 const socket = io('https://localhost:3000')
 const messageForm = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
+
 const messageInput = document.getElementById('message-input')
 
 socket.on('chat-message', data =>{
-console.log(data)
+appendMessage(data)
 
 }
 )
@@ -14,3 +16,8 @@ const message = messageInput.value
 socket.emit('send-chat-message', message)
 messageInput.value = ''
 })
+function appendMessage(message){
+const messageElement = document.createElement('div')
+messageElement.innerText = message
+messageContainer.append(messageElement)
+}
