35f7318ace0779fd23affa39872168106adb3db3
Samrath Jhand <you@example.com>
2022-05-23 15:34:31 -0700

    changes

---
+++ chatscript.js
@@ -0,0 +1,16 @@
+const socket = io('https://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+socket.on('chat-message', data =>{
+console.log(data)
+
+}
+)
+
+messageForm.addEventListener('submit', e => {
+e.preventDefault()
+const message = messageInput.value
+socket.emit('send-chat-message', message)
+messageInput.value = ''
+})
